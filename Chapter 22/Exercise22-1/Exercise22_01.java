36ba16ae98d8707428805979319a662b19008c6b
Joshua Gray <86368706+Joshawott64@users.noreply.github.com>
2021-10-07 09:39:06 -0600

    Updated Exercise22_01.java

--- Chapter 22/Exercise22-1/Exercise22_01.java
+++ Chapter 22/Exercise22-1/Exercise22_01.java
@@ -7,6 +7,7 @@
  */
 import java.util.Scanner;
 import java.util.*;
+import java.awt.*;
 
 public class Exercise22_01 {
 
@@ -14,29 +15,37 @@ public class Exercise22_01 {
 		// Set up scanner
 		Scanner input = new Scanner(System.in);
 		
+		// Create lists
+		LinkedList<Character> max = new LinkedList<>();
+		LinkedList<Character> list = new LinkedList<>();
+		
 		// Prompt user
 		System.out.print("Enter a string: ");
 		String userString = input.nextLine();
 		
 		// Output maximum consecutive substring
-		System.out.println("Maximum consecutive substring is " + maxSubstring(userString));
+		System.out.println("Maximum consecutive substring is " + maxSubstring(userString, max, list));
 	}
 	
-	public static String maxSubstring(String userString) {
-		// Convert userString to ArrayList of characters
-		ArrayList<Character> chars = new ArrayList<Character>();
-		for (char c: userString.toCharArray()) {
-			chars.add(c);
-		}
-		
-		StringBuilder stringBuilder = new StringBuilder();
-		for (int i = 0, j = 1; i < chars.size() && j < chars.size(); i++, j++) {
-			if (chars.get(i) < chars.get(j)) {
-				System.out.println("WORKING ON IT");
+	public static String maxSubstring(String userString, LinkedList<Character> max, LinkedList<Character> list) {
+		for (int i = 0; i < userString.length(); i++) {
+			if (list.size() > 1 && userString.charAt(i) <= list.getLast() && list.contains(userString.charAt(i))) {
+				list.clear();
+			}
+			
+			list.add(userString.charAt(i));
+			
+			if (list.size() > max.size()) {
+				max.clear();
+				max.addAll(list);
 			}
 		}
 		
-		return userString;
+		StringBuilder finishedString = new StringBuilder();
+		for (Character ch: max) {
+			finishedString.append(ch);
+		}
+		return finishedString.toString();
 	}
 	
 }
