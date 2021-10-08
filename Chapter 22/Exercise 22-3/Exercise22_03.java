090d2b8fb7f7cfe18450823d7a65a5e52293b499
Joshua Gray <86368706+Joshawott64@users.noreply.github.com>
2021-10-08 08:24:28 -0600

    Updated Exercise22_03.java

--- Chapter 22/Exercise 22-3/Exercise22_03.java
+++ Chapter 22/Exercise 22-3/Exercise22_03.java
@@ -1,7 +1,7 @@
 /*
 * Author: Joshua Gray
 * Date: 10/7/21
-* 
+*
 * This program prompts the user to enter two strings and determines whether or not the second string 
 * is a substring of the first and returns the index of the substring if it is.
 */
@@ -22,20 +22,28 @@ public class Exercise22_03 {
 		
 		// Test for substring
 		int index = 0;
-		for (int i = 0, j = 1; i < s1.length() && j < s2.length(); i++) {
+		for (int i = 0, j = 0; i < s1.length() && j < s2.length(); i++) {
 			// Find first letter index
 			if (s2.charAt(0) == s1.charAt(i) && s1.contains(s2)) {
 				index = i;
 			}
-			// Check if letters match after index
-			if (s2.charAt(j) == s1.charAt(i)) {
+			// Check if chars match after index
+			if (s2.charAt(j) == s1.charAt(i) && s1.contains(s2)) {
 				j++;
 			}
+			// Reset j if chars do not match
 			else {
-				index = i;
+				j = 0;
 			}
 		}
 		
-		System.out.println("matched at " + index);
+		System.out.println("matched at index " + index);
 	}
 }
+
+/*
+* Time complexity analysis:
+*
+* It is a constant time, c for executing the if statements.
+* Since the for-loop is executed n times, the time complexity of the loop is T(n) = (c) * n = O(n).
+*/
