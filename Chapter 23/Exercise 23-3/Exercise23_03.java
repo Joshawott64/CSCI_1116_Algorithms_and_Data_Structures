f0aa04a9e7a9b163dc16f404aa81cdf52fa64a6b
Joshua Gray <86368706+Joshawott64@users.noreply.github.com>
2021-10-11 10:38:27 -0600

    Updated Exercise23_03.java

--- Chapter 23/Exercise 23-3/Exercise23_03.java
+++ Chapter 23/Exercise 23-3/Exercise23_03.java
@@ -1,8 +1,7 @@
 /*
-Author: 
-Date: 
-
-Description: 
+Author: Joshua Gray
+Date: 10/11/21
+Description: This program sorts arrays with generic methods that use quick sort.
 */
 import java.util.Comparator;
 
@@ -25,10 +24,111 @@ public class Exercise23_03 {
   }
   
   public static <E extends Comparable<E>> void quickSort(E[] list) {
-    
+       quickSort(list, 0, list.length - 1);
+  }
+  
+  public static <E extends Comparable<E>> void quickSort(E[] list, int first, int last) {
+	  if (last > first) {
+		  int pivotIndex = partition(list, first, last);
+		  quickSort(list, first, pivotIndex - 1);
+		  quickSort(list, pivotIndex + 1, last);
+	  }
+  }
+  
+  // Partition the array list[]
+  public static <E extends Comparable<E>> int partition(E[] list, int first, int last) {
+	  E pivot = list[first];
+	  int low = first + 1;
+	  int high = last;
+	  
+	  while (high > low) {
+		  // Search forward from left
+		  while (low <= high && (list[low].compareTo(pivot) == -1 || 
+				  list[low].compareTo(pivot) == 0)) {
+			  low++;
+		  }
+		  
+		  // Search backward from right
+		  while (low <= high && list[high].compareTo(pivot) == 1) {
+			  high--;
+		  }
+		  
+		  // Swap two elements in the list
+		  if (high > low) {
+			  E temp = list[high];
+			  list[high] = list[low];
+			  list[low] = temp;
+		  }
+	  }
+	  
+	  while (high > first && (list[high].compareTo(pivot) == 1 || 
+			  list[high].compareTo(pivot) == 0)) {
+		  high--;
+	  }
+	  
+	  // Swap pivot with list[high]
+	  if (pivot.compareTo(list[high]) == 1) {
+		  list[first] = list[high];
+		  list[high] = pivot;
+		  return high;
+	  }
+	  else {
+		  return first;
+	  }
   }
   
   public static <E> void quickSort(E[] list, Comparator<? super E> comparator) {
-    
+    quickSort(list, comparator, 0, list.length - 1);
   }
+  
+  public static <E> void quickSort(E[] list, Comparator<? super E> comparator, int first, int last) {
+	  if (last > first) {
+		  int pivotIndex = partition(list, comparator, first, last);
+		  quickSort(list, comparator, first, pivotIndex - 1);
+		  quickSort(list, comparator, pivotIndex + 1, last);
+	  }
+  }
+  
+  // Partition the array list[]
+  public static <E> int partition(E[] list, Comparator<? super E> comparator, int first, int last) {
+	  E pivot = list[first];
+	  int low = first + 1;
+	  int high = last;
+	  
+	  while (high > low) {
+		  // Search forward from left
+		  while (low <= high && (comparator.compare(list[low], pivot) == -1 || 
+				  comparator.compare(list[low], pivot) == 0)) {
+			  low++;
+		  }
+		  
+		  // Search backward from right
+		  while (low <= high && comparator.compare(list[high], pivot) == 1) {
+			  high--;
+		  }
+		  
+		  // Swap two elements in the list
+		  if (high > low) {
+			  E temp = list[high];
+			  list[high] = list[low];
+			  list[low] = temp;
+		  }
+	  }
+	  
+	  while (high > first && (comparator.compare(list[high], pivot) == 1 || 
+			  comparator.compare(list[high], pivot) == 0)) {
+		  high--;
+	  }
+	  
+	  // Swap pivot with list[high]
+	  if (comparator.compare(pivot, list[high]) == 1) {
+		  list[first] = list[high];
+		  list[high] = pivot;
+		  return high;
+	  }
+	  else {
+		  return first;
+	  }
+  }
+  
 }
